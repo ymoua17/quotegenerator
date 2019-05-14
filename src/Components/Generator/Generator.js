4a9bf871508b2e814079aab64b1c1e70301dd743
ymoua17 <44180276+ymoua17@users.noreply.github.com>
2019-05-14 18:18:43 -0500

    update

--- src/Components/Generator/Generator.js
+++ src/Components/Generator/Generator.js
@@ -5,7 +5,7 @@ const Generator = ({ requestNewButton, author, quote }) => {
     const url = `https://twitter.com/intent/tweet?text="${quote} - ${author}`;
     return(
         <div className="vh-100 dt w-100">
-            <div className="dtc v-mid">
+            <div className="mt6">
                 <h1 className="mb underline f1-l crystalblue fw3">Breaking Bad Quote Generator</h1>
                 {/* eslint-disable-next-line */}
                 <a href="https://github.com/ymoua17/quotegenerator" target="_blank" className="no-underline white-80 grow dib fw2">*Created by ymoua17*</a>
@@ -14,7 +14,7 @@ const Generator = ({ requestNewButton, author, quote }) => {
                     {/* eslint-disable-next-line */}
                     <button className="pa2 bg-blue mh2 grow b--white-50"><a href={url} target="_blank" className="no-underline white-80">Tweet Me!</a></button>
                 </div>    
-                 <div className="f2-l"> 
+                 <div className="f2-l v-mid"> 
                     <h2 className="crystalblue fw2 mt0">"{quote}"</h2>
                     <h3 className="i white-80 fw2">- {author}</h3>
                 </div>
